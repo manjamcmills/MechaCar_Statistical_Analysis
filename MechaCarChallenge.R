89e6d22439f43e9577c1277078b5cdcb7bf58657
Manja McMills <manjamcmills@gmail.com>
2022-09-23 10:14:44 -0700

    Del#1

---
+++ MechaCarChallenge.R
@@ -0,0 +1,5 @@
+library(dplyr)
+library(tidyverse)
+mechaCar_mpg <- read.csv(file="MechaCar_mpg.csv", check.names=F, stringsAsFactors = F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCar_mpg)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCar_mpg)) #generate summary statistics
