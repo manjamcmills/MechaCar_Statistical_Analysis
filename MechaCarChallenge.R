c12cae150d48587009fed74c1577566f6d24f4ad
Manja McMills <manjamcmills@gmail.com>
2022-10-02 09:04:33 -0700

    update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,7 +2,7 @@
 
 #Use the library() function to load the dplyr package.
 library(dplyr)
-library(tidyverse)
+
 
 #Import and read in the MechaCar_mpg.csv file as a dataframe.
 mechaCar_mpg <- read.csv(file="MechaCar_mpg.csv", check.names=F, stringsAsFactors = F)
@@ -13,9 +13,74 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance
 
 #Using the summary() function, determine the p-value and the r-squared value for the 
 #linear regression model.
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance 
+mult_lines_summary <-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance 
            + AWD, data=mechaCar_mpg)) #generate summary statistics
+#Show a graph for each of the linear models
+library(tidyverse)
+#vehicle_length
+model1<-lm(mpg ~ vehicle_length, data=mechaCar_mpg)
+summary1<-summary(lm(mpg ~ vehicle_length, data=mechaCar_mpg))
+yvals <- model1$coefficients['vehicle_length']*mechaCar_mpg$vehicle_length + model1$coefficients['(Intercept)']
+plt1<-ggplot(mechaCar_mpg, aes(x=vehicle_length, y=mpg))
+coeff <- round(model1$coefficients, 2)
+pval <- round(summary1$coefficients, 10)
+rval <- round(summary(model1)$r.squared, 4)
+theme1 <- theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5), plot.caption = element_text(hjust = 0.5)  )
+line_eq_1 <- labs(title = "Vehicle Length vs MPG", subtitle = sprintf("Linear Regression Model: y = %s + %s*x", coeff[1], coeff[2]), 
+                  caption = sprintf("R-sqaured: %s       p-value: %s", rval, pval[8]))
+plt1 + geom_point() + geom_line(aes(y=yvals), color="red") + theme1 + line_eq_1
 
+#vehicle_weight
+model1<-lm(mpg ~ vehicle_weight, data=mechaCar_mpg)
+summary1<-summary(lm(mpg ~ vehicle_weight, data=mechaCar_mpg))
+yvals <- model1$coefficients['vehicle_weight']*mechaCar_mpg$vehicle_weight + model1$coefficients['(Intercept)']
+plt1<-ggplot(mechaCar_mpg, aes(x=vehicle_weight, y=mpg))
+coeff <- round(model1$coefficients, 4)
+pval <- round(summary1$coefficients, 10)
+rval <- round(summary(model1)$r.squared, 4)
+theme1 <- theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5), plot.caption = element_text(hjust = 0.5)  )
+line_eq_1 <- labs(title = "Vehicle Weight vs MPG", subtitle = sprintf("Linear Regression Model: y = %s + %s*x", coeff[1], coeff[2]), 
+                  caption = sprintf("R-sqaured: %s       p-value: %s", rval, pval[8]))
+plt1 + geom_point() + geom_line(aes(y=yvals), color="red") + theme1 + line_eq_1
+
+#spoiler_angle
+model1<-lm(mpg ~ spoiler_angle, data=mechaCar_mpg)
+summary1<-summary(lm(mpg ~ spoiler_angle, data=mechaCar_mpg))
+yvals <- model1$coefficients['spoiler_angle']*mechaCar_mpg$spoiler_angle + model1$coefficients['(Intercept)']
+plt1<-ggplot(mechaCar_mpg, aes(x=spoiler_angle, y=mpg))
+coeff <- round(model1$coefficients, 4)
+pval <- round(summary1$coefficients, 10)
+rval <- round(summary(model1)$r.squared, 4)
+theme1 <- theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5), plot.caption = element_text(hjust = 0.5)  )
+line_eq_1 <- labs(title = "Spoiler Angle vs MPG", subtitle = sprintf("Linear Regression Model: y = %s + %s*x", coeff[1], coeff[2]), 
+                  caption = sprintf("R-sqaured: %s       p-value: %s", rval, pval[8]))
+plt1 + geom_point() + geom_line(aes(y=yvals), color="red") + theme1 + line_eq_1
+
+#ground_clearance 
+model1<-lm(mpg ~ ground_clearance , data=mechaCar_mpg)
+summary1<-summary(lm(mpg ~ ground_clearance , data=mechaCar_mpg))
+yvals <- model1$coefficients['ground_clearance']*mechaCar_mpg$ground_clearance  + model1$coefficients['(Intercept)']
+plt1<-ggplot(mechaCar_mpg, aes(x=ground_clearance , y=mpg))
+coeff <- round(model1$coefficients, 4)
+pval <- round(summary1$coefficients, 10)
+rval <- round(summary(model1)$r.squared, 4)
+theme1 <- theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5), plot.caption = element_text(hjust = 0.5)  )
+line_eq_1 <- labs(title = "Ground Clearance vs MPG", subtitle = sprintf("Linear Regression Model: y = %s + %s*x", coeff[1], coeff[2]), 
+                  caption = sprintf("R-sqaured: %s       p-value: %s", rval, pval[8]))
+plt1 + geom_point() + geom_line(aes(y=yvals), color="red") + theme1 + line_eq_1
+
+#AWD
+model1<-lm(mpg ~ AWD, data=mechaCar_mpg)
+summary1<-summary(lm(mpg ~ AWD, data=mechaCar_mpg))
+yvals <- model1$coefficients['AWD']*mechaCar_mpg$AWD  + model1$coefficients['(Intercept)']
+plt1<-ggplot(mechaCar_mpg, aes(x=AWD, y=mpg))
+coeff <- round(model1$coefficients, 4)
+pval <- round(summary1$coefficients, 10)
+rval <- round(summary(model1)$r.squared, 4)
+theme1 <- theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5), plot.caption = element_text(hjust = 0.5)  )
+line_eq_1 <- labs(title = "AWD vs MPG", subtitle = sprintf("Linear Regression Model: y = %s + %s*x", coeff[1], coeff[2]), 
+                  caption = sprintf("R-sqaured: %s       p-value: %s", rval, pval[8]))
+plt1 + geom_point() + geom_line(aes(y=yvals), color="red") + theme1 + line_eq_1
 
 # Deliverable #2
 
@@ -34,5 +99,15 @@ lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mea
                                                Variance = var(PSI), SD = sd(PSI))
 
 
+# Deliverable #3
 
+# write an RScript using the t.test() function to determine if the PSI across all 
+#    manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(suspension_coil$PSI, mu = 1500)
 
+# Next, write three more RScripts in your MechaCarChallenge.RScript using the t.test() function 
+# and its subset() argument to determine if the PSI for each manufacturing lot is statistically 
+# different from the population mean of 1,500 pounds per square inch.
+t.test(subset(suspension_coil, Manufacturing_Lot == "Lot1")$PSI, mu =1500)
+t.test(subset(suspension_coil, Manufacturing_Lot == "Lot2")$PSI, mu =1500)
+t.test(subset(suspension_coil, Manufacturing_Lot == "Lot3")$PSI, mu =1500)
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # MechaCar_Statistical_Analysis
+
+
+
+## Linear Regression to Predict MPG (Deliverable #1)
+Using R, a linear model was designed to see if any of the following metrics could predict the MPG of MechaCar prototypes:
+* vehicle length
+* vehicle weight
+* spoiler angle
+* drivetrain
+* ground clearance
+  
+Below is a screenshot of the multiple linear regression results: 
+![](Resources/mult_line_summary_stats.png)
+
+
+The only 2 variables/coefficients that provided a non-random amount (p-value< .05) of variance to the mpg values in the dataset, were the following:
+
+1. Vehicle Length with p-value of 2.60e-12 
+2. Ground Clearance with p-value of 5.21e-08 <br>
+ **Vehicle weight was close with a p-value of 0.0776*
+
+ The slope of the model is not considered to be zero since the Multiple R-squared (0.7149) and Adjusted R-Squared (0.6825) both show the model is a good predictor (>0.5). However, the following had individual slopes that were close to zero:
+ * Vehicle_weight had a slope of 1.245e-03
+ * Spoiler_angle had a slope of 6.877e-02
+
+This multiple linear regression model is a "somewhat '' good predictor to predict mpg of MechaCar prototypes since the R-squared values are both higher than 0.5 and the p-value of 5.35e-11 is less than 0.05.  
+
+However, I feel that looking at each metric individually compared to MPG can give a better predictor.  The following shows each metric with a "singular" linear regression model, along with each R-sqaured and  p-value:
+
+![](Resources/all_metrics_single_linear_reg.png)
+
+
+
+## Summary Statistics on Suspension Coils (Deliverable #2)
+
+### The design specifications for the MechaCar suspension coils dictate that the variance of the suspension coils must not exceed 100 pounds per square inch. Does the current manufacturing data meet this design specification for all manufacturing lots in total and each lot individually? Why or why not?
